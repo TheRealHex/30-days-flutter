db2c6f14d377977add76ffa5267735c2a865c815
TheRealHex <devilxnp@gmail.com>
2024-01-09 18:57:24 +0545

    Day 1

--- Day1.dart
+++ Day1.dart
@@ -4,39 +4,55 @@
 //   ondrive,
 // } //Enumeration of constants; List of constants
 
+// void main() {
+// const - Used for compile time constants
+// final - Used at runtime, memory usage only when used/called
+
+// var carStatus = Status.stop;
+// if (carStatus == Status.stop) {
+//   print('Stopped');
+// }
+// doSomething();
+// print(calcArea(3, 4));
+
+// Data types int, float, String, double, num, var, bool
+
+// Learnt about Map
+// Map user = {
+//   "name": "TheRealHex",
+//   "Place": "Somewhere",
+// };
+
+// Key value pairs printing using for in
+// for (var i in user.values) {
+//   print(i);
+// }
+
+// Map Entry print
+// for (var i in user.entries) {
+//   print(i.key);
+//   print(i.value);
+// }
+
+// user.clear(); // clears all values
+
+// user.forEach(
+//   (key, value) => print(key + " " + value),
+// ); // foreach keyvalue pairs print
+// }
+
+// class A {
+//   // static const or final for classes
+// }
+
+// Class in dart
+class Product {
+  Product(String name) {
+    //constructor
+    print("Product: $name");
+  }
+}
+
 void main() {
-  // const - Used for compile time constants
-  // final - Used at runtime, memory usage only when used/called
-
-  // var carStatus = Status.stop;
-  // if (carStatus == Status.stop) {
-  //   print('Stopped');
-  // }
-  // doSomething();
-  // print(calcArea(3, 4));
-
-  // Data types int, float, String, double, num, var, bool
-
-  // Learnt about Map
-  // Map user = {
-  //   "name": "TheRealHex",
-  //   "Place": "Somewhere",
-  // };
-
-  // Key value pairs printing using for in
-  // for (var i in user.values) {
-  //   print(i);
-  // }
-
-  // Map Entry print
-  // for (var i in user.entries) {
-  //   print(i.key);
-  //   print(i.value);
-  // }
-
-  // user.clear(); // clears all values
-
-  // user.forEach(
-  //   (key, value) => print(key + " " + value),
-  // ); // foreach keyvalue pairs print
+  Product P = Product('Chips'); // Object initialization
 }

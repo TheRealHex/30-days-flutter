b759c071b34801a2c80f1c6ad1e46c47e98ac4c2
TheRealHex <devilxnp@gmail.com>
2024-01-09 17:45:38 +0545

    Day 1

---
+++ Day1.dart
@@ -0,0 +1,39 @@
+// enum Status {
+//   start,
+//   stop,
+//   ondrive,
+// } //Enumeration of constants; List of constants
+
+void main() {
+  // var carStatus = Status.stop;
+  // if (carStatus == Status.stop) {
+  //   print('Stopped');
+  // }
+  // doSomething();
+  // print(calcArea(3, 4));
+
+  // Data types int, float, String, double, num, var, bool
+
+  // Learnt about Map
+  // Map user = {
+  //   "name": "TheRealHex",
+  //   "Place": "Somewhere",
+  // };
+
+  // Key value pairs printing using for in
+  // for (var i in user.values) {
+  //   print(i);
+  // }
+
+  // Map Entry print
+  // for (var i in user.entries) {
+  //   print(i.key);
+  //   print(i.value);
+  // }
+
+  // user.clear(); // clears all values
+
+  // user.forEach(
+  //   (key, value) => print(key + " " + value),
+  // ); // foreach keyvalue pairs print
+}

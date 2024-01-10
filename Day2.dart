042442bc47dd7c51ede4b83bbc88c37aa351566d
TheRealHex <devilxnp@gmail.com>
2024-01-10 21:30:41 +0545

    Day 2

--- Day2.dart
+++ Day2.dart
@@ -1,12 +1,15 @@
-// general class
-class Area<T> {
-  calcArea(T l, T b) {
-    print(int.parse(l.toString()) * int.parse(b.toString()));
-  }
-}
+// // general class
+// class Area<T> {
+//   calcArea(T l, T b) {
+//     print(int.parse(l.toString()) * int.parse(b.toString()));
+//   }
+// }
 
-void main() {
-  Area rectangle = Area();
-  rectangle.calcArea(2, 3);
-  rectangle.calcArea('2', '3');
-}
+// void main() {
+//   Area rectangle = Area();
+//   rectangle.calcArea(2, 3);
+//   rectangle.calcArea('2', '3');
+// }
+
+// Maps Generic, uses <String, String> by default.
+// Maps<String, dynamic> can be also used

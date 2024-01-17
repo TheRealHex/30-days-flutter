f7c2b4af7a197f5cb4154f44dec620cabc68cc5d
TheRealHex <devilxnp@gmail.com>
2024-01-17 19:17:44 +0545

    Responsive

--- todo_app/lib/responsive/responsive.dart
+++ todo_app/lib/responsive/responsive.dart
@@ -4,18 +4,25 @@ import 'package:todo_app/responsive/mobile_view.dart';
 import 'package:todo_app/responsive/tablet_view.dart';
 
 class ResponsiveUI extends StatelessWidget {
-  const ResponsiveUI({super.key});
+  final Widget mobileScaffold;
+  final Widget tabletScaffold;
+  final Widget desktopScaffold;
+  const ResponsiveUI(
+      {required this.mobileScaffold,
+      required this.tabletScaffold,
+      required this.desktopScaffold,
+      super.key});
 
   @override
   Widget build(BuildContext context) {
     return LayoutBuilder(
       builder: (context, constraints) {
         if (constraints.maxWidth < 600) {
-          return const MobileView();
+          return mobileScaffold;
         } else if (constraints.maxWidth < 900) {
-          return const TabletView();
+          return tabletScaffold;
         } else {
-          return const DesktopView();
+          return desktopScaffold;
         }
       },
     );
--- todo_app/lib/screen/home.dart
+++ todo_app/lib/screen/home.dart
@@ -1,11 +1,18 @@
 import 'package:flutter/material.dart';
+import 'package:todo_app/responsive/desktop_view.dart';
+import 'package:todo_app/responsive/mobile_view.dart';
 import 'package:todo_app/responsive/responsive.dart';
+import 'package:todo_app/responsive/tablet_view.dart';
 
 class Home extends StatelessWidget {
   const Home({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const ResponsiveUI();
+    return const ResponsiveUI(
+      mobileScaffold: MobileView(),
+      tabletScaffold: TabletView(),
+      desktopScaffold: DesktopView(),
+    );
   }
 }
